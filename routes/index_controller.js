dd4847cb8ce3833180e7c8d2e5325fd05d9a4558
Alan <alanvianamendes@outlook.com>
2020-03-26 22:11:01 -0300

    update deps

--- routes/index_controller.js
+++ routes/index_controller.js
@@ -5,7 +5,7 @@ let Express = require("express")
 let Router = Express.Router();
 
 Router.get("/", function (req, res, next) {
-	res.send("Index Page Controller")
+	res.send("Index Page Controller :D")
 });
 
 Router.get('/pug', (req, res, next) => {
